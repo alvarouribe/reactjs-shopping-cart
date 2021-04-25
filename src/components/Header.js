1f90d74a60fa6cf72cfda257aa5cac7a58731edb
Alvaro Uribe <iam@alvarouribe.cl>
2021-04-25 12:51:36 +1200

    transfer styles to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,34 @@ import React from 'react';
 export default function Header(props) {
   const { countCartItems } = props;
 
+  const headerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '1rem',
+    margin: '0.5rem',
+    borderRadius: '0.5rem',
+  };
+
+  const badgeStyle = {
+    backgroundColor: 'rgba(142, 137, 219, 1)',
+    border: 'none',
+    color: 'white',
+    width: '1.5rem',
+    borderRadius: '3rem',
+    textAlign: 'center',
+  }
+
   return (
-    <header className="row block center">
+    <header style={ headerStyle }>
       <div>
-        <h1>Shopping cart</h1>
+        <h1>MVM Shopping cart</h1>
       </div>
       <div>
         <span>
         Cart { ' ' }
         { countCartItems ? (
-          <button className="badge">{ countCartItems }</button>
+          <button style={ badgeStyle }>{ countCartItems }</button>
         ) : ( ' ' ) }
         </span>{ ' ' }
       </div>
